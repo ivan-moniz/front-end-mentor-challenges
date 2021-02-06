a18759f6cd7fb54d49dbd4053e06c64f66567a63
ivan-moniz <ivangrandmoniz@gmail.com>
2021-02-06 17:14:32 -0100

    committed javascript file

---
+++ script.js
@@ -0,0 +1,10 @@
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "80px",
+  duration: 2000,
+  reset: true,
+});
+
+sr.reveal(".first", { delay: 100 });
+sr.reveal(".container__first", { delay: 600 });
