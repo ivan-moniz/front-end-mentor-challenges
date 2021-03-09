6c38bdcc8747e0b3e578e8e5a9c5ec31d803713f
ivan-moniz <ivangrandmoniz@gmail.com>
2021-03-09 22:32:32 -0100

    changed javascript file

--- easybank-landing-page-master/js/main.js
+++ easybank-landing-page-master/js/main.js
@@ -1,19 +1,15 @@
 const menu = document.getElementById("menu");
-const navBar = document.querySelector('nav');
-let header = document.querySelector('header');
+const navBar = document.querySelector("nav");
+let header = document.querySelector("header");
 
 menu.addEventListener("click", () => {
-    
-    if(navBar.style.display === 'none') {
-        navBar.style.display = 'flex';
-        menu.classList.add('showClose');
-       
-    } else {
-        navBar.style.display = 'none';
-        menu.classList.remove('showClose');
-    }
-    
-    console.log('123');
+  if (navBar.style.display === "none") {
+    navBar.style.display = "flex";
+    menu.classList.add("showClose");
+  } else {
+    navBar.style.display = "none";
+    menu.classList.remove("showClose");
+  }
+
+  console.log("123");
 });
-
-
