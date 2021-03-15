8bbe80b9621942533c32d3728fe495da917afd92
ivan-moniz <ivangrandmoniz@gmail.com>
2021-03-15 22:15:12 -0100

    added loop studios to latest project

--- loopstudios-landing-page-main/script.js
+++ loopstudios-landing-page-main/script.js
@@ -1,6 +1,12 @@
-const menu = document.getElementById('menu');
-const nav = document.querySelector('.header__links');
+const menu = document.getElementById("menu");
+const nav = document.querySelector(".header__links");
 
-menu.addEventListener('click', () => {
-    nav.style.display = 'block';
+menu.addEventListener("click", () => {
+  if (nav.style.display == "none") {
+    nav.style.display = "block";
+    menu.classList.add("show__close");
+  } else {
+    nav.style.display = "none";
+    menu.classList.remove("show__close");
+  }
 });
