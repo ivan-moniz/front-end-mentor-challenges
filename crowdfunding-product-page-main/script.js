9c495f7d0d5638ed3eb70b80ee7d57033b38597c
ivan-moniz <ivangrandmoniz@gmail.com>
2021-03-29 18:06:33 -0100

    commit changes to javaScript

--- crowdfunding-product-page-main/script.js
+++ crowdfunding-product-page-main/script.js
@@ -1,10 +1,25 @@
-const modal = document.querySelector('.modal__container');
-const openModal = document.getElementById('open__modal');
+const modal = document.querySelector(".modal__container");
+const openModal = document.getElementById("open__modal");
+const closeModal = document.getElementById("close__modal");
+const bookmark = document.getElementById("bookmarked");
+let spanMark = document.getElementById("bookmark");
 
-openModal.addEventListener('click', () => {
-    if(modal.style.display == 'none') {
-        modal.style.display = 'flex';
-    } else {
-        modal.style.display = 'none';
-    }
+openModal.addEventListener("click", () => {
+  if (modal.style.display == "none") {
+    modal.style.display = "flex";
+  } else {
+    modal.style.display = "none";
+  }
+});
+
+closeModal.addEventListener("click", () => {
+  if (modal.style.display == "flex") {
+    modal.style.display = "none";
+  } else {
+    modal.style.display = "flex";
+  }
+});
+
+bookmark.addEventListener("click", () => {
+  spanMark.innerText("Bookmarked");
 });
