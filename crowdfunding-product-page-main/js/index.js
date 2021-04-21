d6e2178f55e305c372d163129865a5d16e30354f
ivan-moniz <ivangrandmoniz@gmail.com>
2021-04-21 17:51:05 -0100

    set of changes made

--- crowdfunding-product-page-main/js/index.js
+++ crowdfunding-product-page-main/js/index.js
@@ -1,5 +1,54 @@
 import saveBookmark from "./script.js";
+import { openModal, closeModal, addPledgeField } from "./script.js";
+
+// get elements from html
+document.addEventListener("click", modalStuff);
+
+function modalStuff(event) {
+  const modalBg = document.querySelector(".modal__container"),
+    modal = document.querySelector(".modal"),
+    modalSuccess = document.querySelector(".success__modal"),
+    target = event.target,
+    paidEdition = document.querySelectorAll(".paid__edition");
+
+  if (target.classList.contains("select")) {
+    openModal(modalBg, modal);
+  } else if (target.classList.contains("modal__close")) {
+    closeModal(modalBg, modal);
+  } else if (target.classList.contains("btn__checkbox")) {
+    const selected = document.querySelectorAll(".modal__pledge"),
+      card = target.parentElement.parentElement.parentElement;
+
+    selected.forEach((card) => {
+      card.classList.remove("selected");
+      card.childNodes[1].childNodes[1].classList.remove("selected");
+      card.childNodes[4].innerHTML = "";
+    });
+
+    if (!target.parentElement.parentElement.classList.contains("faded")) {
+      target.classList.add("selected");
+      card.classList.add("selected");
+    }
+
+    if (card.classList.contains("bamboo__stand")) {
+      addPledgeField(card, 25);
+    } else if (card.classList.contains("black__stand")) {
+      addPledgeField(card, 75);
+    } else {
+    }
+  } else if (target.classList.contains("go-to-success")) {
+    closeModal(modalBg, modal);
+    openModal(modalBg, modalSuccess);
+  } else if (target.classList.contains("close-success-modal")) {
+    closeModal(modalBg, modalSuccess);
+  }
+
+  paidEdition.forEach(element =>
+    element.addEventListener('submit', () => {
+      closeModal(modalBg, modal);
+      openModal(modalBg, modalSuccess);
+    })
+  );
+}
 
 saveBookmark();
-
-
--- crowdfunding-product-page-main/js/script.js
+++ crowdfunding-product-page-main/js/script.js
@@ -1,52 +1,46 @@
 // elements selection
-const modal = document.querySelector(".modal__container");
-const openModal = document.querySelectorAll(".select");
-const closeModal = document.getElementById("close__modal");
-const selection = document.querySelectorAll(".selection");
-const pledge = document.querySelectorAll('.pledge__bottom');
 const btnBookmark = document.querySelector(".btn__bookmark");
 let spanMark = document.getElementById("bookmark");
 
+btnBookmark.addEventListener("click", saveBookmark);
 
-btnBookmark.addEventListener('click', saveBookmark);
-
-
-
-// open modal code
-openModal.forEach( button => {
-  button.addEventListener('click' , () => {
-    modal.classList.add("show")
-  })
-});
-
-// close modal statement
-closeModal.addEventListener("click", () => {
-  modal.classList.remove("show");
-});
-
-// selection code 
-selection.forEach(select => {
-  select.addEventListener('click', () => {
-    pledge.forEach(bottom => {
-      bottom.classList.add('active');
-    })
-    
-  });
-});
-
-// open modal
-
-
-// bookmark code 
-export default function saveBookmark() {
-
-  if (btnBookmark.classList.contains('saved')) {
-    btnBookmark.classList.remove('saved');
-    spanMark.textContent = 'Bookmark';
-  } else {
-    btnBookmark.classList.add('saved');
-    spanMark.textContent = 'Bookmarked';
-  }
-
+export function openModal(modalBg, modal) {
+  modalBg.classList.add("show");
+  modal.classList.add("active");
 }
 
+export function closeModal(modalBg, modal) {
+  modalBg.classList.remove("show");
+  modal.classList.remove("active");
+}
+
+export function addPledgeField(card, amount) {
+  const pledgeArea = card.childNodes[5];
+
+  if (card.classList.contains("card-no-reward")) {
+    pledgeArea.innerHTML = `
+    <div class="no-reward-pledge-area">
+      <a class="btn-primary go-to-success" href="#">Continue</a>
+    </div>`;
+  } else {
+    pledgeArea.innerHTML = `
+    <div class="pledge-content">
+      <p class="primary-paragraph">Enter your pledge</p>
+      <form class="continue-form">
+        <input type="number" value="${amount}" min="${amount}" />
+        <button class="btn-primary" type="submit">Continue</button>
+      </form>
+    </div>`;
+  }
+}
+
+// bookmark code
+export default function saveBookmark() {
+  if (btnBookmark.classList.contains("saved")) {
+    btnBookmark.classList.remove("saved");
+    spanMark.textContent = "Bookmark";
+  } else {
+    btnBookmark.classList.add("saved");
+    spanMark.textContent = "Bookmarked";
+  }
+}
